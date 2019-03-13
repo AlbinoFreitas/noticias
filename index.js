9d38678acaa5c3714deebeeed6f9279be25292a9
AlbinoFreitas <albino_freitas@hotmail.com>
2019-03-13 06:06:07 -0300

    criar usuário admin no start da aplicação

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -18,12 +19,24 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+const createInitialUser = async () => {
+    const total = await User.countDocuments({ username: 'albinofreitas' })
+    if(total === 0){
+        const user = new User({
+            username: 'albinofreitas',
+            password: '123456'
+        })
+        await user.save()
+    }
+}
+
 mongoose
     .connect(mongo, { useNewUrlParser: true })
     .then(() => {
         app.listen(port, (err) => {
             if(!err){
                 console.log('server running')
+                createInitialUser() 
             }
         })
     })
