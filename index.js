221d5881ee409b20cd039a5711f2fa8b86906fe9
AlbinoFreitas <albino_freitas@hotmail.com>
2019-03-14 07:05:40 -0300

    criar e organizar rotas

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const path = require('path')
+const path       = require('path')
 const bodyParser = require('body-parser')
-const express = require('express')
-const mongoose = require('mongoose')
-const User = require('./models/user')
+const express    = require('express')
+const mongoose   = require('mongoose')
+const session    = require('express-session')
 
-const port = process.env.PORT || 3000
-const app = express()
-const mongo = process.env.MONGO || 'mongodb://localhost/noticias'
+const User     = require('./models/user')
+const noticia  = require('./routes/noticia')
+const restrito = require('./routes/restrito')
+const auth     = require('./routes/auth')
+const pages    = require('./routes/pages')
+
+const app        = express()
+const port       = process.env.PORT || 3000
+const mongo      = process.env.MONGO || 'mongodb://localhost/noticias'
 mongoose.Promise = global.Promise
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(session({ 
+    secret: 'fullstack-master', 
+    resave: true, 
+    saveUninitialized: true 
+}))
+
+app.use('/restrito', (req, res, next) => {
+    if('user' in req.session){
+        return next()
+    }
+    res.redirect('/login')
 })
+
+app.use('/restrito', restrito)
+app.use('/noticias', noticia)
+app.use('/login', auth)
+app.use('/', pages)
 
 const createInitialUser = async () => {
     const total = await User.countDocuments({ username: 'albinofreitas' })
@@ -35,7 +54,7 @@ mongoose
     .then(() => {
         app.listen(port, (err) => {
             if(!err){
-                console.log('server running')
+                console.log('server running at port: '+port)
                 createInitialUser() 
             }
         })
