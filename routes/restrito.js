2114183e46a2a786f79092029a7ba10ed8425044
AlbinoFreitas <albino_freitas@hotmail.com>
2019-03-20 06:31:05 -0300

    listar noticias privadas

--- routes/restrito.js
+++ routes/restrito.js
@@ -1,7 +1,19 @@
 const express = require('express')
 
+const Noticia = require('../models/noticia')
+
 const router = express.Router()
 
-router.get('/noticias', (req, res) => res.send('noticias restritas'))
+router.use((req, res, next) => {
+    if('user' in req.session){
+        return next()
+    }
+    res.redirect('/login')
+})
+
+router.get('/noticias', async (req, res) => {
+    const noticias = await Noticia.find({ category: 'private'})
+    res.render('noticias/restrito', { noticias })
+})
 
 module.exports = router
